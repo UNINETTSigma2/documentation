bb0b943041eac84505535067c618662dee06f49d
Espen Tangen <espen.tangen@uit.no>
2018-12-13 10:32:16 +0100

    Update ssh-tunnel-fram.bat; corrected server address.

--- quick/ssh-tunnel-fram.bat
+++ quick/ssh-tunnel-fram.bat
@@ -4,7 +4,7 @@
 #
 
 # Change USERNAME to the username on fram.
-plink.exe -L 5901:localhost:5901 USERNAME@desktop.fram.uit.no
+plink.exe -L 5901:localhost:5901 USERNAME@desktop.fram.sigma2.no
 
 
 
