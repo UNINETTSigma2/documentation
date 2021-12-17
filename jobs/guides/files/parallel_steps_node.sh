eb052e76529838e2be480f0e7636f4121498dcfa
Bjørn-Helge Mevik <b.h.mevik@usit.uio.no>
2021-12-17 11:06:06 +0100

    Adjusted to 20.11.x

--- jobs/guides/files/parallel_steps_node.sh
+++ jobs/guides/files/parallel_steps_node.sh
@@ -10,22 +10,25 @@ set -o errexit
 set -o nounset
 
 # Load MPI module
-module purge
-module load OpenMPI/3.1.1-GCC-7.3.0-2.30
+module --quiet purge
+module load OpenMPI/4.1.1-GCC-11.2.0
 module list
 
 # This is needed for job types that hand out whole nodes:
-export SLURM_MEM_PER_CPU=1920
-# (This is for Fram.  For betzy, use 1960.)
+unset SLURM_MEM_PER_NODE
+export SLURM_MEM_PER_CPU=1920 # This is for Fram.  For betzy, use 1960.
+
+# This is needed with the current version of Slurm (20.11.x):
+export SLURM_JOB_NUM_NODES=1-$SLURM_JOB_NUM_NODES
 
 # The set of parallel runs:
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
-srun --ntasks=16 --exclusive ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
+srun --ntasks=16 --exact ./my-binary &
 
 wait
